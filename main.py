46fc87bd2835e322ae94163a78ee9808ef663201
guifornagiero <guifornagiero.c@hotmail.com>
2025-02-15 14:41:14 -0300

    remove

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ pergunta = input("como posso te ajudar?")
 resposta, intencao = myChatBot.chatbot_response(pergunta)
 print(resposta + "   ["+intencao[0]['intent']+"]")
 
-
 while (intencao[0]['intent']!="despedida"):
     pergunta = input("posso lhe ajudar com algo a mais?")
     resposta, intencao = myChatBot.chatbot_response(pergunta)
